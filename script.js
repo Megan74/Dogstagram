18b2cb2d5fed12b2629d3976f1ac18ea3f3e644d
Megan74 <97180810+Megan74@users.noreply.github.com>
2022-02-10 23:17:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/* global Vue */
+
+const app = new Vue({
+  el: "#app",
+  data: {
+   currentDogUrl: null,
+    favourites: []
+  },
+  methods: {
+    loadDog: async function() {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+      const asJson = await response.json();
+      this.currentDogUrl = asJson.message;
+      console.log(this.currentDogUrl);
+    },
+    addFav: function() {
+      if (!this.favourites.includes(this.currentDogUrl)) {
+      this.favourites.push(this.currentDogUrl);
+    }
+      this.loadDog();
+    },
+    removeFav: function(dog) {
+      this.favourites = this.favourites.filter(item => item !== dog);
+    }
+  },
+  created() {
+    this.loadDog();
+  },
+  mounted() {
+    if (localStorage.favourites) {
+      this.favourites = JSON.parse(localStorage.favourites);
+    }
+  },
+  watch:{
+    favourites(updatedFavourites) {
+      localStorage.favourites = JSON.stringify(updatedFavourites);
+    }
+  }
+});
+
+
+
